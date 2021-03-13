ac2ac77ec79a960a516ec6e3d4f5c1533b99bd9d
Orlando <orlando.junior07@hotmail.com>
2021-03-13 17:44:41 -0300

    Iniciando os estudos React

---
+++ appOne/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class AppOne extends Component {
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>Estudo React</Text>
+          <Text>Estudando React-Native</Text>
+        </View>
+        <View>
+          <Text>Inicio</Text>
+          <Text>Olá Mundo</Text>
+        </View>
+      </View>
+    );
+  }
+}
+/*
+export default function AppOne(){
+  return (
+    <View>
+      <View>
+        <Text>Estudo React</Text>
+        <Text>Estudando React-Native</Text>
+      </View>
+      <View>
+        <Text>Inicio</Text>
+        <Text>Olá Mundo</Text>
+      </View>
+    </View>
+  );
+}
+*/
+
+
